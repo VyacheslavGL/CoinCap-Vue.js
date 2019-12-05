a9204fb0b47fe886c3b65b41aa134529ebdc944d
Slava <slava@example.com>
2019-12-05 16:35:25 +0300

    install express

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/'
+      : '/'
 };
+
+// https://cli.vuejs.org/ru/config/#publicpath
+
+// vue-cli-service inspect > output.js можно проинспектировать код webpack
